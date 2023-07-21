1a1a792f54e2a283aeba2879155630d3903d5e84
Dmitry Kiselev <omigoneo@gmail.com>
2023-07-21 22:48:56 +0300

    Pin version to 1.3.0

--- src/Bambins.ApiShip/Client/ApiDefaults.cs
+++ src/Bambins.ApiShip/Client/ApiDefaults.cs
@@ -8,6 +8,6 @@
         internal const string DEFAULT_DATETIME_FORMAT = "yyyy-MM-dd";
         internal const string DEFAULT_PROD_BASE_PATH = "https://api.apiship.ru/v1/";
         internal const string DEFAULT_SANDBOX_BASE_PATH = "http://api.dev.apiship.ru/v1/";
-        internal const string DEFAULT_USER_AGENT = "bambins-apiship-sdk/1.2.0";
+        internal const string DEFAULT_USER_AGENT = "bambins-apiship-sdk/1.3.0";
     }
 }
